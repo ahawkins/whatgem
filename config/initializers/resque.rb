d98ae3e9fe18f25d48235cb975dc917b489ff01d
Adam Hawkins <Adman1965@gmail.com>
2011-02-06 01:14:52 -0800

    ERB inside yaml files doesnt work

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,11 @@
 require 'resque'
 
 resque_config = YAML.load_file(Rails.root.join('config', 'resque.yml'))
-Resque.redis = resque_config[Rails.env]
+
+if Rails.env.production?
+  Resque.redis = ENV["REDISTOGO_URL"]
+else
+  Resque.redis = resque_config[Rails.env]
+end
 
 require 'resque/server'
