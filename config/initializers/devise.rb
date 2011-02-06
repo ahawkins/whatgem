31ddf3a274e0452e2b82f8f517c702645f976a5e
Adam Hawkins <Adman1965@gmail.com>
2011-02-06 00:42:34 -0800

    Production github oauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -168,7 +168,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :github, '1a4bf9835c581055d9cf', '7a1dc633cbaea6aed55356ceddb090ec196dfb5f'
+  config_file = YAML.load_file Rails.root.join('config', 'github_oauth.yml')
+  github = config_file[Rails.env]
+
+  config.omniauth :github, github['client_id'], github['secret']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
