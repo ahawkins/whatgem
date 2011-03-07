431f8400e8c01e3953952cd5e5e3ee5e7c9c2831
Adman65 <Adman1965@gmail.com>
2011-03-07 00:21:58 -0800

    Bye by redis to go

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,10 +2,6 @@ require 'resque'
 
 resque_config = YAML.load_file(Rails.root.join('config', 'resque.yml'))
 
-if Rails.env.production?
-  Resque.redis = ENV["REDISTOGO_URL"]
-else
-  Resque.redis = resque_config[Rails.env]
-end
+Resque.redis = resque_config[Rails.env]
 
 require 'resque/server'
