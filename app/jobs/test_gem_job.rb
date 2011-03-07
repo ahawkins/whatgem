2bd1d85430fb21986028b9f724f331755c7666a9
Adman65 <Adman1965@gmail.com>
2011-03-07 15:19:35 -0800

    Update test job

--- app/jobs/test_gem_job.rb
+++ app/jobs/test_gem_job.rb
@@ -7,12 +7,9 @@ class TestGemJob
     repo_name = ruby_gem.github_url.split('/').last
 
     bash_script = Rails.root.join('bash','test_repo.sh')
-    log_path = Rails.root.join('tmp','test_logs',"#{repo_name}.log")
 
-    cmd = "#{bash_script} #{repo_url} > #{log_path}"
-    %x(#{bash_script} #{repo_url} > #{log_path})
+    ruby_gem.test_log = %x{#{bash_script} #{repo_url}}
 
-    ruby_gem.test_log = File.read log_path
     ruby_gem.save!
   end
 end
