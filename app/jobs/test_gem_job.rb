5e4884f0e8312b0b210e69451e317ee7f589a68b
Adman65 <Adman1965@gmail.com>
2011-03-07 14:46:46 -0800

    Doh

--- app/jobs/test_gem_job.rb
+++ app/jobs/test_gem_job.rb
@@ -1,7 +1,7 @@
 class TestGemJob
   @queue = :high
 
-  def perform(name)
+  def self.perform(name)
     ruby_gem = RubyGem.named name
     repo_url = ruby_gem.github_url.chomp.gsub(/https?/,'git') + '.git'
     repo_name = ruby_gem.github_url.split('/').last
@@ -10,7 +10,6 @@ class TestGemJob
     log_path = Rails.root.join('tmp','test_logs',"#{repo_name}.log")
 
     cmd = "#{bash_script} #{repo_url} > #{log_path}"
-    puts "Running: #{cmd}"
     %x(#{bash_script} #{repo_url} > #{log_path})
 
     ruby_gem.test_log = File.read log_path
